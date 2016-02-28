69b32d2700f02d5d12436438dff332b8ee947576
Iain D Broadfoot <ibroadfo@gmail.com>
2016-02-28 23:14:59 +0000

    plug rotten service into model

--- app/models/post.js
+++ app/models/post.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  rottenCoder: Ember.inject.service(),
   content: Ember.computed({
     set(key, value) {
-      this.set('encodedContent', 'XXXXXXXXXXXXX')
+      this.set('encodedContent', this.get('rottenCoder').encode(value))
       return value;
     }
   })
--- tests/unit/models/post-test.js
+++ tests/unit/models/post-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('post', 'Unit | Model | post', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['service:rottenCoder']
 });
 
 test('it exists', function(assert) {
@@ -14,4 +14,11 @@ test('it exists', function(assert) {
 test('should only store encoded version of content', function(assert) {
   let post = this.subject({content:'allergies'});
   assert.notEqual(post.get('encodedContent', 'allergies'))
+})
+
+test('encoded content should be decodable', function(assert) {
+    let post = this.subject({content:'allergies'});
+    assert.notEqual(post.get('encodedContent', 'allergies'))
+
+    assert.equal('allergies', post.get('rottenCoder').decode(post.get('encodedContent')))
 })
