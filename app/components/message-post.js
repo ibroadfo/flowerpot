b80c34eba3f4a7d0e9230c28b9f499b4a52877f0
Iain D Broadfoot <ibroadfo@gmail.com>
2016-02-29 14:46:47 +0000

    decode button, add visible content property to post component

--- app/components/message-post.js
+++ app/components/message-post.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  rottenCoder: Ember.inject.service(),
+  visibleContent: Ember.computed.oneWay('model.encodedContent'),
+  actions: {
+    decodeMe() {
+      console.log('ok, well this is progress');
+      this.set('visibleContent', this.get('rottenCoder').decode(this.get('model.encodedContent')))
+    }
+  }
 });
--- tests/integration/components/message-post-test.js
+++ tests/integration/components/message-post-test.js
@@ -20,15 +20,15 @@ test('it renders, but not the plaintext', function(assert) {
   assert.equal(this.$().text().trim(), 'onqtre shpxre');
 });
 
-// test('clicking decode decodes the text', function(assert) {
-//   this.set('model2', server.create('post', {encodedContent:'onqtre shpxre'}))
-//
-//   this.render(hbs`{{message-post model=model}}`);
-//
-//   assert.notEqual(this.$().text().trim(), 'badger fucker');
-//
-//   this.$('button').click();
-//
-//   assert.equal(this.$().text().trim(), 'badger fucker');
-//
-// });
+test('clicking decode decodes the text', function(assert) {
+  this.set('model2', server.create('post', {encodedContent:'onqtre shpxre'}))
+
+  this.render(hbs`{{message-post model=model2}}`);
+
+  assert.notEqual(this.$().text().trim(), 'badger fucker');
+
+  this.$('button').click();
+
+  assert.equal(this.$('#post').text().trim(), 'badger fucker');
+
+});
