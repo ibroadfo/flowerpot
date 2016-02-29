c2363d7f378032a79bb75c39ec228be5e6312b02
Iain D Broadfoot <ibroadfo@gmail.com>
2016-02-29 14:05:38 +0000

    we have to pretend mirage is the right server type

--- app/mirage/factories/post.js
+++ app/mirage/factories/post.js
@@ -2,5 +2,5 @@ import Mirage, {faker} from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
   author(i) { return faker.internet.userName(); },
-  encodedContent: 'onqtre shpxre'
+  encoded_content: 'onqtre shpxre'
 });
