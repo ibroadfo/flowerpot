1292c4ac94dd996b21947e26caf5d9bcd9304dce
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-01 16:06:11 +0000

    also grab an display the post's title

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     submitTheThing() {
-      let p = this.get('store').createRecord('post', {content: $('textarea.content').val()});
+      let p = this.get('store').createRecord('post', {content: $('textarea.content').val(), title:$('input.title').val()});
       p.save().then( () => {
         this.transitionTo('post', p);
       });
