5f58e99f200db0972f5ced1076d4a4b3cf9fcc06
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-07 14:29:29 +0000

    kill logging

--- app/components/message-post.js
+++ app/components/message-post.js
@@ -5,7 +5,6 @@ export default Ember.Component.extend({
   visibleContent: Ember.computed.oneWay('model.encodedContent'),
   actions: {
     decodeMe() {
-      console.log('ok, well this is progress');
       this.set('visibleContent', this.get('rottenCoder').decode(this.get('model.encodedContent')));
       this.$('button').prop('disabled', true);
     }
