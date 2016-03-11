b0b8dda9d6922b3945a805e4a2916acae0777c44
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-11 14:31:18 +0000

    add whitespace to make readable

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -3,9 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     submitTheThing() {
-      let p = this.get('store').createRecord('post', {content: $('textarea.content').val(),
-      title:$('input.title').val(),
-      note: $('input.note').val()});
+      let p = this.get('store').createRecord('post', {
+          content: $('textarea.content').val(),
+          title:$('input.title').val(),
+          note: $('input.note').val()
+        }
+      );
       p.save().then( () => {
         this.transitionTo('post', p);
       });
