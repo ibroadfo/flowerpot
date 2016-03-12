23cfac80c4cb27ebb617f6c88ebcd6a889d099e7
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-12 10:06:40 +0000

    lose post.author

--- app/mirage/factories/post.js
+++ app/mirage/factories/post.js
@@ -4,7 +4,6 @@ import Mirage, {faker} from 'ember-cli-mirage';
 export default Mirage.Factory.extend({
   rottenCoder: Ember.inject.service(),
 
-  author() { return faker.internet.userName(); },
   encoded_content() { return $.rot13(faker.lorem.paragraph()); },
   title() { return faker.company.catchPhrase(); },
   note() { return faker.lorem.words(); },
