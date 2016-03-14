4e8d634309bb73f78c4102c5fc7769256e4d52ba
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-14 03:29:52 +0000

    toggle able abuse reporting

--- app/components/report-abuse.js
+++ app/components/report-abuse.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'span',
   classNames: ['report-abuse'],
   currentURL: Ember.computed(function() {
     return encodeURIComponent(window.location);
   }),
+  formVisible: false,
+  buttonText: Ember.computed('formVisible', function() {
+    if (this.get('formVisible')) {
+      return 'hide abuse form';
+    }
+    else {
+      return 'report abuse';
+    }
+  }),
+  actions: {
+    toggleAbuseForm() {
+      this.toggleProperty('formVisible');
+    }
+  }
 });
