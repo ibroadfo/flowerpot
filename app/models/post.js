e29c03e560c3b4e36cec5606b6af5de2d3e2b05b
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-14 09:31:21 +0000

    need to define a get otherwise Stuff Goes Wrong

--- app/models/post.js
+++ app/models/post.js
@@ -4,6 +4,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   rottenCoder: Ember.inject.service(),
   content: Ember.computed({
+    get() {
+      return '';
+    },
     set(key, value) {
       this.set('encodedContent', this.get('rottenCoder').encode(value));
       return value;
