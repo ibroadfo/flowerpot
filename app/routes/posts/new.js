b23a915b84917e371de365c852e0bc5918506af1
Iain D Broadfoot <ibroadfo@gmail.com>
2016-04-10 02:02:14 +0100

    use flash addon to tell user their post has been created

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
   model() {
     return this.store.createRecord('post');
   },
@@ -8,6 +9,7 @@ export default Ember.Route.extend({
     submitTheThing(thing) {
       thing.set('timestamp', new Date().getTime());
       thing.save().then( () => {
+        this.get('flashMessages').add({message:'this is your new post; you can share this page with whoever you like.', sticky:true});
         this.transitionTo('post', thing);
       });
     }
