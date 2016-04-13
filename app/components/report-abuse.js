7f1308463e835a69e7daf6e71d5da277fb93b9a8
Iain D Broadfoot <ibroadfo@gmail.com>
2016-04-13 06:16:57 +0100

    make abuse report button a less obtrusive link

--- app/components/report-abuse.js
+++ app/components/report-abuse.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['report-abuse'],
   extraContext: '',
+  tagName: 'span',
   actions: {
     submitReport() {
       this.sendAction('createReport', {
