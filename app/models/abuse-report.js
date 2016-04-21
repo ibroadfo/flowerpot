4b1d55bb7c4439eec8c1ba05a5d47eadcf6b6d78
Iain D Broadfoot <ibroadfo@gmail.com>
2016-04-21 10:59:13 +0100

    explicit async false

--- app/models/abuse-report.js
+++ app/models/abuse-report.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   extraContext: DS.attr(),
-  post: DS.belongsTo('post'),
+  post: DS.belongsTo('post', {async: false}),
   timestamp: DS.attr('number')
 
 });
