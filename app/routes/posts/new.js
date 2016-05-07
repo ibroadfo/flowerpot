93ebfd582a67741fca724aa2f58023fdb00a75c3
Iain D Broadfoot <ibroadfo@gmail.com>
2016-05-07 14:21:18 +0100

    move post creation action from route into controller

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -4,14 +4,5 @@ export default Ember.Route.extend({
   flashMessages: Ember.inject.service(),
   model() {
     return this.store.createRecord('post');
-  },
-  actions: {
-    submitTheThing(thing) {
-      thing.set('timestamp', new Date().getTime());
-      thing.save().then( () => {
-        this.get('flashMessages').add({message:'this is your new post; you can share this page with whoever you like.', sticky:true});
-        this.transitionTo('post', thing);
-      });
-    }
   }
 });
