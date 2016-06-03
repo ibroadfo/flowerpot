8b39796d03a5dfeb59481435111ac23d74179f82
Iain D Broadfoot <ibroadfo@gmail.com>
2016-06-03 16:50:36 +0100

    selectively hide header&footer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,5 +4,13 @@ import config from '../config/environment';
 export default Ember.Controller.extend({
   showIndex: Ember.computed('property', function() {
     return config.APP.showIndex;
-  })
+  }),
+  classForCurrentPath: Ember.computed('currentPath', {
+    get() {
+      let currentPath = this.get('currentPath');
+      if (currentPath) {
+        return `${currentPath.replace(/\./g, '-')}-route`;
+      }
+    }
+  }),
 });
