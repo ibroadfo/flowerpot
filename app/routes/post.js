b1687ad9b769320d80330d47e288f425430c6595
Ivana McConnell <bilic.ivana@gmail.com>
2016-07-09 18:36:41 -0700

    Adjusting warning note phrasing
    Trying to encourage one note rather than several tags

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     if(params.post_id === 'example'){
-      return {title:'Example Post Title',note:'Warning Note', encodedContent:'Guvf vf fbzr rknzcyr pbagrag. Abeznyyl, guvf pbhyq or n oybt cbfg, n abgr, be nal xvaq bs pbagrag be cvrpr bs grkg gung arrqf gur nccyvrq jneavat. Vs lbh pna ernq guvf, lbh unir pbafragrq gb qrpbqvat vg.'};
+      return {title:'Example Post Title',note:'This is only an example post with no offensive content, so it does not need this warning, but other Flowerpot posts might.', encodedContent:'Guvf vf fbzr rknzcyr pbagrag. Abeznyyl, guvf pbhyq or n oybt cbfg, n abgr, be nal xvaq bs pbagrag be cvrpr bs grkg gung arrqf gur nccyvrq jneavat. Vs lbh pna ernq guvf, lbh unir pbafragrq gb qrpbqvat vg.'};
     }
     return this.store.findRecord('post', params.post_id);
   }
