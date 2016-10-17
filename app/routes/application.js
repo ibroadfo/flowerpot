fdfee13019de8642e498851f1bd973f44e4b5933
Iain D Broadfoot <ibroadfo@gmail.com>
2016-10-17 11:51:04 +0100

    disable twitter session stuff unless firebase is being used

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,19 +3,25 @@ import ResetScrollMixin from 'ember-cli-reset-scroll';
 
 export default Ember.Route.extend(ResetScrollMixin, {
   beforeModel: function() {
-    return this.get("session").fetch().catch(function() {});
+    if (this.features.isEnabled('use-firebase')) {
+      return this.get("session").fetch().catch(function() {});
+    }
   },
 
   actions: {
     signIn: function(provider) {
-      this.get("session").open("firebase", { provider: provider}).then((data) => {
-        console.log(data);
-        this.refresh();
-      });
+      if (this.features.isEnabled('use-firebase')) {
+        this.get("session").open("firebase", { provider: provider}).then((data) => {
+          console.log(data);
+          this.refresh();
+        });
+      }
     },
     signOut: function() {
-      this.get("session").close();
-      this.refresh();
+      if (this.features.isEnabled('use-firebase')) {
+        this.get("session").close();
+        this.refresh();
+      }
     }
   }
 });
